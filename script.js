54e547fb33adc9bfe9b6859580713f920dd407b6
Balajiw920 <wbalaji2021@gmail.com>
2023-01-13 10:14:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ function palindrome(str){
     let s = str.toLowerCase().replace(/[^a-z0-9]/g, "");
     return s === s.split("").reverse().join("");
 }
-console.log(isPalindrome("racecar")); // Output: true
-console.log(isPalindrome("RaceCar")); // Output: true
-console.log(isPalindrome("raceCAR")); // Output: true
-console.log(isPalindrome("hello")); // Output: false
+console.log(palindrome("racecar")); // Output: true
+console.log(palindrome("RaceCar")); // Output: true
+console.log(palindrome("raceCAR")); // Output: true
+console.log(palindrome("hello")); // Output: false
 
 module.exports = palindrome
